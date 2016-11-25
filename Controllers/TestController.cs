6bb3308e8c04e1a5043a6ee528a9d0794ba65c60
Julius Weyl <julius.weyl@haw-hamburg.de>
2016-11-25 14:55:13 +0100

    Controller for test purposes added

---
+++ Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BestandService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace BestandService.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+
+
+        /// <summary>
+        /// Get a list of all known stations including
+        /// the position, name and stock of the stations
+        /// </summary>
+        /// <returns>List of all bike stations</returns>
+        [HttpGet]
+        public string GetAll()
+        {
+            const string bestand = "http://localhost:5000/bestand";
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(bestand).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // by calling .Result you are performing a synchronous call
+                    var responseContent = response.Content;
+
+                    // by calling .Result you are synchronously reading the result
+                    var responseString = responseContent.ReadAsStringAsync().Result;
+
+                    Console.WriteLine(responseString);
+
+                    return responseString;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
