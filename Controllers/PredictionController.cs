902aedecea66da99eec5c38012fd079c1f7fb15a
Julius Weyl <julius.weyl@haw-hamburg.de>
2017-01-14 13:28:31 +0100

    vorläufige Änderungen

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -58,42 +58,65 @@ namespace BestandService.Controllers
                 var stationInfo = stadtradParser.GetInfoForOneStation(receivedInfos, stationName);
                 if (stationInfo != null)
                 {
-                    var response = new HttpResponseMessage();
-                    using (var client = new HttpClient())
+                    bool dummyPredictionTest = true;
+                    if (dummyPredictionTest)
                     {
-                        if (Development)
+                        Random rand = new Random();
+                        int pred = rand.Next(-3, 3);
+                        int current = (int) stationInfo.SelectToken("bikes");
+
+                        JArray hist = new JArray();
+                        hist.Add(rand.Next(0, current));
+                        hist.Add(rand.Next(3, 15));
+                        hist.Add(rand.Next(4, 16));
+                        hist.Add(rand.Next(5, 17));
+
+                        stationInfo.Add(new JProperty("history", hist));
+
+                        int realBikeCount = current + pred;
+                        if (realBikeCount < 0)
+                            realBikeCount = 0;
+                        stationInfo.Add("prediction", realBikeCount);
+                    }
+                    else
+                    {
+                        var response = new HttpResponseMessage();
+                        using (var client = new HttpClient())
                         {
-                            response = client.GetAsync(dummyPrediction).Result;
-                        }
-                        else
-                        {
-                            var encoding = System.Net.WebUtility.UrlEncode(stationName);
-                            var requestedStation = prediction + "?name=" + encoding;
-                            Console.WriteLine("Requested Prediction ULR: " + requestedStation);
-                            response = client.GetAsync(requestedStation).Result;
-                        }
+                            if (Development)
+                            {
+                                response = client.GetAsync(dummyPrediction).Result;
+                            }
+                            else
+                            {
+                                var encoding = System.Net.WebUtility.UrlEncode(stationName);
+                                var requestedStation = prediction + "?name=" + encoding;
+                                Console.WriteLine("Requested Prediction ULR: " + requestedStation);
+                                response = client.GetAsync(requestedStation).Result;
+                            }
 
-                        if (response.IsSuccessStatusCode)
-                        {
-                            //var predictionResponseArray = JArray.Parse(System.IO.File.ReadAllText("prediction.json"));
-                            var predictionResponseArray = JArray.Parse(response.Content.ReadAsStringAsync().Result);
-                            Console.WriteLine("Empfangener Array: " + predictionResponseArray);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                //var predictionResponseArray = JArray.Parse(System.IO.File.ReadAllText("prediction.json"));
+                                var predictionResponseArray = JArray.Parse(response.Content.ReadAsStringAsync().Result);
+                                Console.WriteLine("Empfangener Array: " + predictionResponseArray);
 
-                            int pred = (int)predictionResponseArray[0];
-                            var current = (int) stationInfo.SelectToken("bikes");
+                                int pred = (int) predictionResponseArray[0];
+                                var current = (int) stationInfo.SelectToken("bikes");
 
-                            JArray hist = new JArray();
-                            hist.Add((int)predictionResponseArray[2]);
-                            hist.Add((int)predictionResponseArray[3]);
-                            hist.Add((int)predictionResponseArray[4]);
-                            hist.Add((int)predictionResponseArray[5]);
+                                JArray hist = new JArray();
+                                hist.Add((int) predictionResponseArray[2]);
+                                hist.Add((int) predictionResponseArray[3]);
+                                hist.Add((int) predictionResponseArray[4]);
+                                hist.Add((int) predictionResponseArray[5]);
 
-                            stationInfo.Add(new JProperty("history",hist));
+                                stationInfo.Add(new JProperty("history", hist));
 
-                            int realBikeCount = current + pred;
-                            if (realBikeCount < 0)
-                                realBikeCount = 0;
-                            stationInfo.Add("prediction",realBikeCount);
+                                int realBikeCount = current + pred;
+                                if (realBikeCount < 0)
+                                    realBikeCount = 0;
+                                stationInfo.Add("prediction", realBikeCount);
+                            }
                         }
                     }
 
