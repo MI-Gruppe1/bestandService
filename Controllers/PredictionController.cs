250d4c6ff03db3439dd29148c5788e5d73173dd4
Julius Weyl <julius.weyl@haw-hamburg.de>
2017-01-14 14:26:52 +0100

    Mock Daten losgeworden

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -33,89 +33,95 @@ namespace BestandService.Controllers
             }
             else
             {
-        ///////////////
-        //Quickfix
-        ////////////////
-                receivedInfos = radInfoDownloader.ReadStadtRadResponseFromFile();
-//                receivedInfos = radInfoDownloader.DownloadStadtRadInformation();
-//                if (receivedInfos == null)
-//                {
-//                    throw new HttpRequestException("Stadtrad API not reachable");
-//                }
+                Console.WriteLine("Prediction Controller working in production mode");
+                receivedInfos = radInfoDownloader.DownloadStadtRadInformation();
+                if (receivedInfos == null)
+                {
+                    Console.WriteLine("Prediction Controller: Stadtrad API not reachable");
+                    throw new HttpRequestException("Prediction Controller: Stadtrad API not reachable");
+                }
             }
 
             var stadtradParser = new StadtradParser();
 
             var reqBody = new StreamReader(Request.Body).ReadToEnd();
+            //Console.WriteLine("Prediction Controller: Request Body read: " + reqBody.ToString());
             var stations = JArray.Parse(reqBody);
             var collectedInformation = "";
 
             foreach (var station in stations)
             {
                 var stationName = (string) station.SelectToken("name");
-                Console.WriteLine(stationName);
+                Console.WriteLine("\n\nPrediction Controller: requested Station " +stationName);
 
                 var stationInfo = stadtradParser.GetInfoForOneStation(receivedInfos, stationName);
                 if (stationInfo != null)
                 {
-                    bool dummyPredictionTest = true;
+                    Console.WriteLine("Prediction Controller: using fake prediction to simulate prediction service");
+                    const bool dummyPredictionTest = false;
                     if (dummyPredictionTest)
                     {
-                        Random rand = new Random();
-                        int pred = rand.Next(-3, 3);
-                        int current = (int) stationInfo.SelectToken("bikes");
+                        var rand = new Random();
+                        var pred = rand.Next(-3, 3);
+                        var current = (int) stationInfo.SelectToken("bikes");
 
-                        JArray hist = new JArray();
-                        hist.Add(rand.Next(0, current));
-                        hist.Add(rand.Next(3, 15));
-                        hist.Add(rand.Next(4, 16));
-                        hist.Add(rand.Next(5, 17));
+                        var hist = new JArray
+                        {
+                            rand.Next(0, current),
+                            rand.Next(3, 15),
+                            rand.Next(4, 16),
+                            rand.Next(5, 17)
+                        };
 
                         stationInfo.Add(new JProperty("history", hist));
+                        Console.WriteLine("Prediction Controller: fake history added to stationInfo: " + hist.ToString());
 
-                        int realBikeCount = current + pred;
+                        var realBikeCount = current + pred;
                         if (realBikeCount < 0)
                             realBikeCount = 0;
                         stationInfo.Add("prediction", realBikeCount);
+                        Console.WriteLine("Prediction Controller: fake prediction added to stationInfo: " + realBikeCount);
                     }
                     else
                     {
+                        Console.WriteLine("Prediction Controller: trying to get information from real prediction service");
                         var response = new HttpResponseMessage();
                         using (var client = new HttpClient())
                         {
-                            if (Development)
-                            {
-                                response = client.GetAsync(dummyPrediction).Result;
-                            }
-                            else
-                            {
                                 var encoding = System.Net.WebUtility.UrlEncode(stationName);
                                 var requestedStation = prediction + "?name=" + encoding;
-                                Console.WriteLine("Requested Prediction ULR: " + requestedStation);
+                                Console.WriteLine("Prediction Controller: Requested Prediction ULR: " + requestedStation);
+
+                                //response = new HttpResponseMessage(HttpStatusCode.Accepted);
                                 response = client.GetAsync(requestedStation).Result;
-                            }
 
                             if (response.IsSuccessStatusCode)
                             {
-                                //var predictionResponseArray = JArray.Parse(System.IO.File.ReadAllText("prediction.json"));
+                                Console.WriteLine("Prediction Controller: received IsSuccessfullStatusCode from prediction service");
+                                //var predictionResponseArray = JArray.Parse("[-10.22,11.0,13.0,7.0,12.0,4.0]");
                                 var predictionResponseArray = JArray.Parse(response.Content.ReadAsStringAsync().Result);
-                                Console.WriteLine("Empfangener Array: " + predictionResponseArray);
+                                Console.WriteLine("Prediction Controller: Empfangener Array: " + predictionResponseArray);
 
-                                int pred = (int) predictionResponseArray[0];
+                                var pred = (int) predictionResponseArray[0];
                                 var current = (int) stationInfo.SelectToken("bikes");
+                                Console.WriteLine("Prediction Controller: received prediction: " + pred);
 
-                                JArray hist = new JArray();
-                                hist.Add((int) predictionResponseArray[2]);
-                                hist.Add((int) predictionResponseArray[3]);
-                                hist.Add((int) predictionResponseArray[4]);
-                                hist.Add((int) predictionResponseArray[5]);
+                                var hist = new JArray
+                                {
+                                    (int) predictionResponseArray[2],
+                                    (int) predictionResponseArray[3],
+                                    (int) predictionResponseArray[4],
+                                    (int) predictionResponseArray[5]
+                                };
 
                                 stationInfo.Add(new JProperty("history", hist));
+                                Console.WriteLine("Prediction Controller: history added to stationInfo: " + hist.ToString());
 
                                 int realBikeCount = current + pred;
                                 if (realBikeCount < 0)
                                     realBikeCount = 0;
                                 stationInfo.Add("prediction", realBikeCount);
+                                Console.WriteLine("Prediction Controller: prediction added to stationInfo: " + realBikeCount);
                             }
                         }
                     }
@@ -124,6 +130,7 @@ namespace BestandService.Controllers
                         collectedInformation += "," + stationInfo;
                     else
                         collectedInformation += stationInfo;
+                    Console.WriteLine("Prediction Controller: collected Information: " + collectedInformation.ToString());
                 }
                 else
                 {
